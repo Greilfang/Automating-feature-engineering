a62fd0e612d7231b2bcad1de3e996d9093f1928d
Greilfang <565222945@qq.com>
2019-11-01 02:07:08 +0800

    define transformation class and implement the AFE.py

--- codes/AFE.py
+++ codes/AFE.py
@@ -1,34 +1,41 @@
 import numpy as np
 from sklearn.neural_network import MLPClassifier
 from utils.ReadRecords import read_data_features
-
-
+from utils.transformation import transformations
+import random
+# 得到这是第几个bin
 def get_bin_index(Val,Supr,Infr,Blank):
-    return int(Val-Infr)/Blank
+    return int((Val-Infr)/Blank)
+
+def getScale(feature,BinNum):
+    return None
+'''
+in:整个数据集的data
+'''
+def generate_binary_training_samples(OriginalSet,BinNum,SampleNum):
+    UsefulTag,UselessTag = [1,0],[0,1]
+    col_num = OriginalSet['data'].shape[1]
+    f1,f2 = random.sample([i for i in range(col_num)],2)
+    QuantifiedSketchVector=np.zeros((BinNum,2))
+    QuantifiedSketchVector[:,0]=getScale(OriginalSet['data'][:,f1],BinNum)
+    QuantifiedSketchVector[:,1]=getScale(OriginalSet['data'][:,f2],BinNum)
+    ClassNum = len(np.unique(OriginalSet['target']))
+    for index in range(SampleNum):
+        pass
 
 
-def generate_samples(SubFeatureMap,BinNum,ClassNum):
-    assert(type(SubFeatureMap)==dict)
-    for FName,FValue in SubFeatureMap.keys(),SubFeatureMap.values():
-        QuantifiedFValue=np.zeros((BinNum,ClassNum))
-        Supr,Infr=max(FValue),min(FValue)
-        for val in FValue:
-            Blank=(Supr-Infr)/BinNum
-            idx = get_bin_index(val,Infr,Supr,Blank)
-            #cls = SubFeature
-            QuantifiedFValue[idx]=QuantifiedFValue[idx]+1
-        return QuantifiedFValue
-
-
-
-
+def generate_unary_training_samples(OriginalSet,BinNum,SampleNum):
+    pass
 
 
 
 
 hyparams = {
-    'bucket_num':10
+    'bin_num':10
 }
 
 feature_map=read_data_features('./utils/CleanedData.csv')
-print(feature_map)
+# 加载对应的transformation集合
+Transformations=transformations()
+Transformations.load_transformations()
+#TrainSet = generate_training_samples(feature_map,hyparams['bin_num'])
---
+++ codes/utils/transformation.py
@@ -0,0 +1,138 @@
+# 这个文件是用来定义各种对feature 的变换
+def hi():
+    pass
+class transformations:
+    def __init__(self):
+        self.binary_transformation_map={
+            'sum':None,
+            'subtraction':None,
+            'multiplication':None,
+            'devision':None
+        }
+        self.unary_transformation_map={
+            'log':None,
+            'square_root':None,
+            'frequency':None,
+            'square':None,
+            'round':None,
+            'tanh':None,
+            'sigmoid':None,
+            'isotonic_regression':None,
+            'zscore':None,
+            'normalization':None
+
+        }
+
+
+    '''
+    in: 一个ndarray的2列(m×1)
+    out: 一个m×1 的 1 列
+    description: 2列每个元素对应相加
+    '''
+    def sum(self,column_1,column_2):
+        pass
+    '''
+    in: 一个ndarray的2列(m×1)
+    out: 一个m×1 的 1 列
+    description: 2列每个元素对应相减
+    '''
+    def substract(self,column_1,column_2):
+        pass
+    
+    '''
+    in: 一个ndarray的2列(m×1)
+    out: 一个m×1 的 1 列
+    description: 2列每个元素对应相减
+    '''
+    def multiply(self,column_1,column_2):
+        pass
+
+    '''
+    in:  一个ndarray的2列(m×1)
+    out: 一个m×1 的 1 列
+    description: 2列每个元素对应相减
+    '''
+    def divide(self,column_1,column_2):
+        pass
+    '''
+    in:  一个ndarray 的1列(m×1)
+    out: 一个m×1 的 1 列
+    description: 2列每个元素对应log_2^val
+    '''
+    def log(self,column):
+        pass
+    '''
+    in:  一个ndarray 的1列(m×1)
+    out: 一个m×1 的 1 列
+    description: 2列每个元素对应求平方根,负值对绝对值求平方根加符号,例如square_root(-9)=-3
+    '''
+    def square_root(self,column):
+        pass
+    '''
+    in:  一个ndarray 的1列(m×1)
+    out: 一个m×1 的 1 列
+    description: 对应元素替换成该元素在这一列出现的频次,例:[7,7,2,3,3,4] -> [2,2,1,2,2,1]
+    '''
+    def frequency(self,column):
+        pass
+    '''
+    in:  一个ndarray 的1列(m×1)
+    out: 一个m×1 的 1 列
+    description:每个值对应四舍五入
+    '''
+    def round(self,column):
+        pass
+    '''
+    in:  一个ndarray 的1列(m×1)
+    out: 一个m×1 的 1 列
+    description:每个值对应双曲正切
+    '''
+    def tanh(self,column):
+        pass
+    '''
+    in:  一个ndarray 的1列(m×1)
+    out: 一个m×1 的 1 列
+    description:每个值对应sigmoid,自己查一下sigmoid函数
+    '''
+    def sigmoid(self,column):
+        pass
+    '''
+    in:  一个ndarray 的1列(m×1),
+    out: 一个m×1 的 1 列
+    description:对该列值的分布进行,自己查一下保序回归
+    '''
+    def isotonic_regression(self,column):
+        pass
+    '''
+    in:  一个ndarray 的1列(m×1),
+    out: 一个m×1 的 1 列
+    description:对该列值的分布进行z分数,查一下z分数
+    '''    
+    def zscore(self,column):
+        pass
+    '''
+    in:  一个ndarray 的1列(m×1),
+    out: 一个m×1 的 1 列
+    description:对该列值的分布进行-1到1正则化,查一下normalization
+    '''    
+    def normalize(self,column):
+        pass
+
+    def load_transformations(self):
+        self.unary_transformation_map['sum']=self.sum
+        self.unary_transformation_map['substraction']=self.substract
+        self.unary_transformation_map['multiplication']=self.multiply
+        self.unary_transformation_map['division']=self.divide
+        self.binary_transformation_map['log']=self.log
+        self.binary_transformation_map['square']=self.square_root
+        self.binary_transformation_map['frequency']=self.round
+        self.binary_transformation_map['round']=self.round
+        self.binary_transformation_map['tanh']=self.tanh
+        self.binary_transformation_map['sigmoid']=self.sigmoid
+        self.binary_transformation_map['isotonic_regression']=self.isotonic_regression
+        self.binary_transformation_map['zscore']=self.zscore
+        self.binary_transformation_map['normalization']=self.normalize
+        print('load successfully')
+
+#Transformations = transformations()
+#Transformations.load_transformations()
