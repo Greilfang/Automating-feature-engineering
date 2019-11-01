1781df1420c400521c1379240e8959994296abcb
Greilfang <565222945@qq.com>
2019-11-02 01:06:59 +0800

    complete the transformation of quantified sketch

--- codes/AFE.py
+++ codes/AFE.py
@@ -1,37 +1,85 @@
 import numpy as np
 from sklearn.neural_network import MLPClassifier
 from utils.ReadRecords import read_data_features
-from utils.transformation import transformations
+from utils.transformation import transformations,TransformationDataSets
+from sklearn.ensemble import RandomForestClassifier
 import random
-# 得到这是第几个bin
-def get_bin_index(Val,Supr,Infr,Blank):
-    return int((Val-Infr)/Blank)
+import copy
 
-def getScale(feature,BinNum):
-    return None
-'''
-in:整个数据集的data
-'''
-def generate_binary_training_samples(OriginalSet,BinNum,SampleNum):
+
+
+
+# 得到分位数分布
+def getSketch(feature_t,BinNum,cls_num,classes):
+    QuantifiedSketchVector=np.zeros((BinNum,cls_num))
+    Supr,Infr=max(feature_t),min(feature_t)
+    Blank = Supr-Infr 
+    for fvalue,cvalue in feature_t,classes:
+        idx = int((fvalue-Infr)/Blank*BinNum)
+        QuantifiedSketchVector[idx,cvalue]=QuantifiedSketchVector[idx,cvalue]+1
+    return QuantifiedSketchVector
+
+def splitDataSet(DataSet,ratio):
+    rows=DataSet['data'].shape[0]
+    train_num=int(rows * ratio)
+    TrainSet,TestSet={},{}
+    TrainSet['data']=DataSet['data'][:train_num]
+    TrainSet['target']=DataSet['target'][train_num:]
+    TestSet['data']=DataSet['data'][train_num:]
+    TestSet['target']=DataSet['target'][train_num:]
+    return TrainSet,TestSet
+
+def getTransformedSet(DataSet,features,feature_t):
+    EstimatedSet=copy.deepcopy(DataSet)
+    EstimatedSet['data']=np.delete(EstimatedSet['data'],features,axis=1)
+    EstimatedSet['data']=np.insert(EstimatedSet['data'],0,values=feature_t,axis=1)
+    return EstimatedSet
+
+
+
+def generate_binary_training_samples(TransformationDataSets,Transformations,OriginalSet,BinNum,SampleNum,Improvement):
+    # 分割数据集为训练集和测试集
+    OriginalTrainSet,OriginalTestSet=splitDataSet(OriginalSet,0.8)
+    # 计算基准准确率
+    BenchClassifier=RandomForestClassifier(n_estimators=10)
+    BenchClassifier.fit(OriginalTrainSet['data'],OriginalTrainSet['target'])
+    BenchScore = BenchClassifier.score(OriginalTestSet['data'],OriginalTestSet['target'])
+    # 两个标签,左边是有用,右边是无用
     UsefulTag,UselessTag = [1,0],[0,1]
     col_num = OriginalSet['data'].shape[1]
-    f1,f2 = random.sample([i for i in range(col_num)],2)
-    QuantifiedSketchVector=np.zeros((BinNum,2))
-    QuantifiedSketchVector[:,0]=getScale(OriginalSet['data'][:,f1],BinNum)
-    QuantifiedSketchVector[:,1]=getScale(OriginalSet['data'][:,f2],BinNum)
     ClassNum = len(np.unique(OriginalSet['target']))
-    for index in range(SampleNum):
-        pass
+    for sample in range(SampleNum):
+        f1,f2 = random.sample([i for i in range(col_num)],2)
+        feature_1,feature_2=OriginalSet['data'][:,f1],OriginalSet['data'][:,f2]
+        #遍历每一种变换，为每一个MLP增加训练样本
+        for trans_name,trans_verb in Transformations.binary_transformation_map.values():
+            feature_t=trans_verb(feature_1,feature_2)
+            QuantifiedSketchVector=getSketch(feature_t,BinNum,ClassNum,OriginalSet['target'])
+            #产生转化后的数据集
+            EstimatedSet=getTransformedSet(OriginalSet,[f1,f2],col_num)
+            EstimatedTrainSet,EstimatedTestSet=splitDataSet(EstimatedSet,0.8)
 
+            BenchClassifier.fit(EstimatedSet['data'],EstimatedSet['target'])
+            EstimatedScore=BenchClassifier.score(EstimatedTrainSet,EstimatedTestSet)
+            TransformationDataSets[trans_name]['data'].append(QuantifiedSketchVector)
+            if EstimatedScore-BenchScore > Improvement:
+                TransformationDataSets[trans_name]['target'].append(UsefulTag)
+            else:
+                TransformationDataSets[trans_name]['target'].append(UselessTag)
 
 def generate_unary_training_samples(OriginalSet,BinNum,SampleNum):
     pass
 
 
+a=np.array([[1,2,3],[4,5,6],[7,8,9]])
+a=np.delete(a,[0,1],axis=1)
+print(a)
 
 
 hyparams = {
-    'bin_num':10
+    'bin_num':10,
+    'improvement':0.01,
+    'SampleNum':10000
 }
 
 feature_map=read_data_features('./utils/CleanedData.csv')
