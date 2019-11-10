97faef2e06b161dd581db466c83b992fc44827b8
Greilfang <565222945@qq.com>
2019-11-10 16:34:57 +0800

    flat the matrix

--- codes/Training.py
+++ codes/Training.py
@@ -1,4 +1,26 @@
 import pickle
+import numpy as np
 from utils.ReadRecords import load_transformations
-Transformations=load_transformations('./model/transformations')
-#print(Transformations.binary_transformation_map)
+from utils.transformation import transformations
+
+
+#Transformations=load_transformations('./model/transformations')
+#用在AFE.py文件里储存的数据集继续训练
+
+def training(Transformations):
+    TrainingData=Transformations.DataSets
+    #对数据集内数据进行重新设置维数使其输入mlp
+    for trans_name,trans_set in TrainingData.keys(),TrainingData.values():
+        trans_set['data']=list(map(lambda x: x.flatten(),trans_set['data']))
+        trans_set['data']=np.array(trans_set['data'])
+    
+
+if __name__ == "__main__":
+    a = np.array([[1,2,3,4,5],[6,7,8,9,10]])
+    b = np.array([[-1,-2,-3,-4,-5],[-6,-7,-8,-9,-10]])
+    c=[]
+    c.append(a)
+    c.append(b)
+    c = list(map(lambda x: x.flatten(),c))
+    k =np.array(c)
+    print(k)
